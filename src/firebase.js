cf5590effca35e55f781808f51702026d408751f
Chunyue Ma <cma01@wesleyan.edu>
2021-07-08 15:16:21 -0400

    refactor and document functions

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 
-//browser extension's Firebase configuration, connects to Firebase project
+// Firebase configuration, connects to Firebase project
 const firebaseConfig = {
     apiKey: "AIzaSyDDaReuI_p2gS2e-4j6B_JdFk4Lf1gkN88",
     authDomain: "privacy-choice-research.firebaseapp.com",
@@ -9,16 +9,15 @@ const firebaseConfig = {
     appId: "1:23402940855:web:1ee3c7bc69ffdb51b04032",
     measurementId: "G-L6EWBVR01J"
     };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+let currentUserID;
 
-
-
-
-var currentUserID;
+// Function used to create a user in the database
 export async function createUser(){
-    var db=firebase.firestore();
+    let db=firebase.firestore();
     //get largest user ID being used
     let largestUserID;
     await db.collection("largestUserID").doc("largestID").get().then((doc)=>{
@@ -55,91 +54,101 @@ export async function createUser(){
     })
 }
 
-
+// Add user entries into the Firebase
 export function addHistory(site, GPC, applyALLBool, enabledBool, currentUserID){
-    let db=firebase.firestore();
-    // chrome.storage.local.get(["USER_ID"], function(result){
-        // let currentUserID=result.USER_ID
-        let docID;
-        let date=new Date()
-        console.log(currentUserID)
-        console.log(db.collection("users").where("User ID", "==", currentUserID))
-        db.collection("users").where("User ID", "==", currentUserID).get()
-            .then((docArray)=>{
-                docArray.forEach((doc)=>{
-                    docID=doc.id
-                    console.log(docID)
-                })})     
-            .then(()=>{
-                db.collection("users").doc(docID).collection("Browser History").add({
-                    "User ID": currentUserID,
-                    date: date.toLocaleDateString(),
-                    time: date.toLocaleTimeString(),
-                    "Browsing History Entry ID": getHistoryEntryID(),
-                    "Tab ID": getTabID(),
-                    "Referer": null,
-                    "Current Site":  site,
-                    "GPC Current Site Status": GPC,
-                    "GPC Global Status": getGPCGlobalStatus(applyALLBool, enabledBool)
+    let db = firebase.firestore();
+    let docID;
+    let date = new Date()
+    console.log(currentUserID)
+    console.log(db.collection("users").where("User ID", "==", currentUserID))
+    db.collection("users").where("User ID", "==", currentUserID).get()
+        .then((docArray)=>{docArray.forEach((doc)=>{
+                docID = doc.id
+                console.log(docID)
+            })
+        })
+        .then(()=>{
+            db.collection("users").doc(docID).collection("Browser History").add({
+                "User ID": currentUserID,
+                date: date.toLocaleDateString(),
+                time: date.toLocaleTimeString(),
+                "Browsing History Entry ID": getHistoryEntryID(),
+                "Tab ID": getTabID(),
+                "Referer": null,
+                "Current Site":  site,
+                "GPC Current Site Status": GPC,
+                "GPC Global Status": getGPCGlobalStatus(applyALLBool, enabledBool)
         })
     })
-    // })
 }
 
-
+// Get GPC Global Status
 function getGPCGlobalStatus(applyALLBool, enabledBool){
-        if(applyALLBool) return enabledBool;
-        else return "unset"
-      }
+    if(applyALLBool) return enabledBool;
+    else return "unset"
+}
 
+// Get the current tab ID
 function getTabID(){
     return null
 }
 
+// 
 function getHistoryEntryID(){
     return null
 }
 
+// Get the browser version
 function getBrowser(){
     return null
 }
 
+// Get the browser engine version
 function getBrowserEngine(){
     return null
 }
 
+// Get the operating system of the user device
 function getOS(){
     return null
 }
 
+// Get the plugins on the user's browser
 function getPlugins(){
     return null;
 }
 
+// Get the language setting of the user's browser
 function getLanguage(){
     return null;
 }
 
+// Get the time zone of the user
 function getTimeZone(){
     return null;
 }
 
+// Get the JS enabled stats of the user
 function getJSEnabled(){
     return null;
 }
 
+// Get the FirstPartyCookiesEnabled status of the user
 function getFirstPartyCookiesEnabled(){
     return null;
 }
 
+// Get the ThirdPartyCookiesEnabled status of the user
 function getThirdPartyCookiesEnabled(){
     return null;
 }
 
+// Get the domain list from the user browser local storage
 function getDomainList(){
     return null;
 }
 
+// Get the scheme that the user is currently presented it
 function getUIscheme(){
     return currentUserID%5;
 }
