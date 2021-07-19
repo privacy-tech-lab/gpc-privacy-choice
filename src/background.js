76744e65c9b641197ce07f8b8030ef308599d509
Chunyue Ma <57780561+chunyuema@users.noreply.github.com>
2021-07-19 16:59:17 -0400

    Issue 76 (#79)
    * use top level domains for updateSendSignalandDomain
    
    * document and fix the #76
    
    * removed openOptions and use chrome runtime API

--- src/background.js
+++ src/background.js
@@ -1,21 +1,23 @@
-// background.js is the main background script handling OptMeowt's main opt-out functionality
-
+/*
+OptMeowt-Research is licensed under the MIT License
+Copyright (c) 2021 Chunyue Ma, Isabella Tassone, Eliza Kuller, Sebastian Zimmeck
+privacy-tech-lab, https://privacytechlab.org/
+*/
 import {createUser, addHistory, updateDomains, addSettingInteractionHistory, addThirdPartyRequests} from "./firebase.js"
-
-chrome.webRequest.onSendHeaders.addListener(addThirdPartyRequests, {urls: ["<all_urls>"]}, ["requestHeaders", "extraHeaders"]);
 
 // Initializers
 let sendSignal = false;
 let optout_headers = {};
 let currentHostname = null;
 
-// Store DOMAIN_LIST, ENABLED, and DOMAINLIST_ENABLED variables in cache for synchronous access
-// Make sure these are always in sync!
+// Store DOMAIN_LIST, ENABLED, and DOMAINLIST_ENABLED variables in cache for synchronous access: Make sure these are always in sync!
 let enabledCache=true;
 let domainsCache= {};
 let domainlistEnabledCache=true;
 let applyAllCache=false;
 
+// Attach addThirdPartyRequest function to record all the request made to the the thirdy party websites
+chrome.webRequest.onSendHeaders.addListener(addThirdPartyRequests, {urls: ["<all_urls>"]}, ["requestHeaders", "extraHeaders"]);
 
 // Set the initial configuration of the extension
 chrome.runtime.onInstalled.addListener(function (object) {
@@ -27,15 +29,9 @@ chrome.runtime.onInstalled.addListener(function (object) {
   enable();
   createUser().then(
     chrome.storage.local.get(["UI_SCHEME"], function(result){
-
-      //SCHEME B: automatically open up the pop up page + provide a tour
-      if(result.UI_SCHEME==2){
-        chrome.runtime.openOptionsPage(() => {
-        })
-      } else {
-        // Other schemes: disable the tour setting
-        chrome.storage.local.set({FIRST_INSTALLED: false});
-      }
+      // direct the user to options page when in scheme 2
+      if(result.UI_SCHEME==2) chrome.runtime.openOptionsPage(() => {})
+      else chrome.storage.local.set({FIRST_INSTALLED: false});
     })
   );
 });
@@ -50,8 +46,9 @@ chrome.storage.local.get(["DOMAINS", "ENABLED", 'DOMAINLIST_ENABLED', 'APPLY_ALL
 
 // Listener for runtime messages
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.greeting == "ENABLE") sendResponse({farewell: "goodbye"});
+  // disable the extension if get message DISABLE_ALL
   if (request.message == "DISABLE_ALL") disable();
+  // add user's browsing history to the database
   if (request.greeting == "NEW PAGE"){
     let jsEnabled = null;
     let tabId = null;
@@ -65,8 +62,8 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       });
     });
   }
-  //update cache from contentScript.js
-  if (request.greeting == "OPEN OPTIONS") openOptions();
+  // update cache from contentScript.js
+  if (request.greeting == "OPEN OPTIONS") chrome.runtime.openOptionsPage(() => {});
   if (request.greeting == "UPDATE CACHE") setCache(request.newEnabled, request.newDomains, request.newDomainlistEnabled, request.newApplyAll);
   //updates Setting Interaction History from contentScript.js and domainlist-view.js
   if (request.greeting == "INTERACTION") {
@@ -122,21 +119,13 @@ function setCache(enabled='dontSet', domains='dontSet', domainlistEnabled='dontS
   if(applyAll!='dontSet') applyAllCache=applyAll;
 }
 
-function openOptions(){
-  chrome.tabs.create({
-    url: '../options/options.html',
-    active: true
-  })
-}
-
 // Update the sendSignal boolean for the current page
 function updateSendSignalandDomain(){
 
   // update current domain
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
     let url = tabs[0].url;
-    let url_object = new URL(url);
-    let domain=url_object.hostname;
+    let domain = getDomain(url);
     currentHostname = domain;
   });
 
@@ -147,15 +136,12 @@ function updateSendSignalandDomain(){
       if (applyAllCache) sendSignal=enabledCache
       else sendSignal=false
     }
-  }else sendSignal=enabledCache
-
+  } else sendSignal = enabledCache
 }
 
 // Add headers if the sendSignal to true
 function addHeaders (details)  {
-
   updateSendSignalandDomain()
-  // console.log("sent headers" + sendSignal +currentHostname);
   if (sendSignal) {
     for (let signal in optout_headers) {
       let s = optout_headers[signal];
@@ -170,7 +156,6 @@ function addHeaders (details)  {
 // Add dom signal if sendSignal to true
 function addDomSignal (details)  {
   updateSendSignalandDomain();
-  // console.log("sent DOM" + sendSignal +currentHostname);
   if (sendSignal) {
     // From DDG, regarding `Injection into non-html pages` on issue-128
     try { 
@@ -190,4 +175,28 @@ function addDomSignal (details)  {
       runAt: "document_start",
     });
   }
+}
+
+// function used to get the hostname from the current url
+function getHostName(url) {
+  let match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+  if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) return match[2];
+  else return null;
+}
+
+// function used to get the top level domain for the current url
+function getDomain(url) {
+  let hostName = getHostName(url);
+  let domain = hostName;
+  
+  if (hostName != null) {
+      let parts = hostName.split('.').reverse();
+      if (parts != null && parts.length > 1) {
+          domain = parts[1] + '.' + parts[0];
+          if (hostName.toLowerCase().indexOf('.co.uk') != -1 && parts.length > 2) {
+            domain = parts[2] + '.' + domain;
+          }
+      }
+  }
+  return domain;
 }
