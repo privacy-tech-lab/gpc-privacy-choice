2f3599ee341bb2f10f3381437c766aaa1069dfc4
bella.tassone <isabellatassone@billericak12.com>
2021-07-20 16:44:26 -0400

    Changed functionality of universal setting field (#80).

--- src/background.js
+++ src/background.js
@@ -23,6 +23,7 @@ chrome.webRequest.onSendHeaders.addListener(addThirdPartyRequests, {urls: ["<all
 chrome.runtime.onInstalled.addListener(function (object) {
   chrome.storage.local.set({ENABLED: true});
   chrome.storage.local.set({APPLY_ALL: false});
+  chrome.storage.local.set({UV_SETTING: "Off"});
   chrome.storage.local.set({DOMAINLIST_ENABLED: true});
   chrome.storage.local.set({FIRST_INSTALLED: true});
   chrome.storage.local.set({DOMAINS: {}});
