fba12cfc2416e08aa928eb783d08ef62732c5ebc
Chunyue Ma <cma01@wesleyan.edu>
2021-07-25 20:05:27 -0400

    remove unnecessary console log code

--- src/questionnaire.js
+++ src/questionnaire.js
@@ -12,14 +12,11 @@ document.querySelectorAll('.choice').forEach(item => {
     item.addEventListener('click', event => {
         let category = event.target.nextElementSibling.nextElementSibling.innerText;
         userChoices[category] = !userChoices[category];
-        console.log(userChoices); 
     })
 })
 
 // Storage the user's choice in the local storage for future reference, close the tab
 document.querySelector('.submit-choice').onclick = (e) => {
-    console.log(e.target);
-    console.log("Submit Button was clicked");
     chrome.storage.local.set({USER_CHOICES: userChoices}, function(){
         window.close();
     });
