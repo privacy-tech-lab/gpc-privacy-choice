0854cd3d7e2bc0cad3b49ee13d3526f8893d7f9b
Chunyue Ma <cma01@wesleyan.edu>
2021-08-18 15:19:49 -0400

    fix webkit rendering engine detection #87

--- src/firebase.js
+++ src/firebase.js
@@ -33,7 +33,7 @@ export async function createUser(schemeNumber){
             "Latitude": latitude, 
             "Longitude": longitude,
             "Browser": getBrowser(),
-            "Browser Engine": getBrowserEngine(),
+            "Rendering Engine": navigator.appVersion.includes("WebKit") ? "WebKit Engine" : "Other Rendering Engine",
             "OS": getOS(),
             "Plugins": getPlugins(),
             "Language": getLanguage(),
@@ -210,10 +210,6 @@ function getBrowser() {
     return browser;
 }
 
-// Get the browser engine version
-function getBrowserEngine(){
-    return null
-}
 
 // Get the operating system of the user device
 function getOS(){
