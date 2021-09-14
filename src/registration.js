af21133d2d87aff3dd21e411e72c862563a907bf
Chunyue Ma <cma01@wesleyan.edu>
2021-09-14 10:39:45 -0400

    set up the password mechanism #134

--- src/registration.js
+++ src/registration.js
@@ -1,7 +1,7 @@
 import {createUser} from "./firebase.js"
+import {PASSWORD} from "./config.js"
 
 let userProfile = null;
-const PASSWORD = 12345;
 
 // Form validation when the user submits their prolific id and password
 document.querySelector('.submit-choice').onclick = () => {
