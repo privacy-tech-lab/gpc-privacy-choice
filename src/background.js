d7d1f7e9e333bd1cd51c5a0afd8e5e0f4476af3e
Chunyue Ma <cma01@wesleyan.edu>
2021-10-11 13:39:38 -0400

    front end working with current implementation #144

--- src/background.js
+++ src/background.js
@@ -95,246 +95,246 @@ chrome.runtime.onInstalled.addListener(async function (r) {
   }
 );
 
-// // Sets cache value to locally stored values after chrome booting up
-// chrome.storage.local.get(["DOMAINS", "ENABLED", 'DOMAINLIST_ENABLED', 'APPLY_ALL'], function (result){
-//   enabledCache=result.ENABLED;
-//   domainsCache=result.DOMAINS;
-//   domainlistEnabledCache=result.DOMAINLIST_ENABLED;
-//   applyAllCache=result.APPLY_ALL;
-// })
+// Sets cache value to locally stored values after chrome booting up
+chrome.storage.local.get(["DOMAINS", "ENABLED", 'DOMAINLIST_ENABLED', 'APPLY_ALL'], function (result){
+  enabledCache=result.ENABLED;
+  domainsCache=result.DOMAINS;
+  domainlistEnabledCache=result.DOMAINLIST_ENABLED;
+  applyAllCache=result.APPLY_ALL;
+})
 
-// // Listener for runtime messages
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//   if (request === "openOptions") { 
-//     chrome.runtime.openOptionsPage(() => {}) 
-//   }
-//   // add user's browsing history to the database
-//   if (request.greeting == "NEW PAGE"){
-//     chrome.storage.local.get(["APPLY_ALL", "ENABLED", "USER_DOC_ID", "UI_SCHEME"], function(result){
-//       if (result.USER_DOC_ID){
-//         addHistory(request.referrer, request.site, sendSignal, result.APPLY_ALL, result.ENABLED, result.USER_DOC_ID, sender.tab.id, result.UI_SCHEME);
-//       } else {
-//         console.log("Unregistered user: not connected to the database");
-//       }
-//     });
-//   }
-//   // update cache from contentScript.js
-//   if (request.greeting == "UPDATE CACHE") setCache(request.newEnabled, request.newDomains, request.newDomainlistEnabled, request.newApplyAll);
-//   // updates Setting Interaction History from contentScript.js and domainlist-view.js
-//   if (request.greeting == "INTERACTION") {
-//     chrome.storage.local.get( ["USER_DOC_ID", "ORIGIN_SITE"], function(result){
-//       let userDocID = result.USER_DOC_ID;
-//       let originSite = result.ORIGIN_SITE;
-//       if (result.USER_DOC_ID){
-//         addSettingInteractionHistory(request.domain, originSite, userDocID, request.setting, request.prevSetting, request.newSetting, request.universalSetting, request.location, request.subcollection);
-//       } else {
-//         console.log("Unregistered user: not connected to the database");
-//       }
-//     })
-//   }
-//   if (request.greeting == "LEARNING COMPLETED"){
-//     chrome.storage.local.set({"LEARNING": "Just Finished"}, function(){
-//       let alreadyOpen = false;
-//       let extensionID = chrome.runtime.id;
-//       chrome.tabs.query({}, function(tabs) {
-//         for (let i = 0, tab; tab = tabs[i]; i++) {
-//             if (tab.url===("chrome-extension://"+ extensionID + "/options/options.html")) {
-//               chrome.tabs.reload(tab.id, {}, function(){});
-//               chrome.tabs.update(tab.id, {active: true});
-//               alreadyOpen = true;
-//             }
-//         }
-//       });
-//       if (!alreadyOpen){
-//         chrome.runtime.openOptionsPage();
-//       }
-//     })
-//   }
-// });
+// Listener for runtime messages
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request === "openOptions") { 
+    chrome.runtime.openOptionsPage(() => {}) 
+  }
+  // add user's browsing history to the database
+  if (request.greeting == "NEW PAGE"){
+    chrome.storage.local.get(["APPLY_ALL", "ENABLED", "USER_DOC_ID", "UI_SCHEME"], function(result){
+      if (result.USER_DOC_ID){
+        addHistory(request.referrer, request.site, sendSignal, result.APPLY_ALL, result.ENABLED, result.USER_DOC_ID, sender.tab.id, result.UI_SCHEME);
+      } else {
+        console.log("Unregistered user: not connected to the database");
+      }
+    });
+  }
+  // update cache from contentScript.js
+  if (request.greeting == "UPDATE CACHE") setCache(request.newEnabled, request.newDomains, request.newDomainlistEnabled, request.newApplyAll);
+  // updates Setting Interaction History from contentScript.js and domainlist-view.js
+  if (request.greeting == "INTERACTION") {
+    chrome.storage.local.get( ["USER_DOC_ID", "ORIGIN_SITE"], function(result){
+      let userDocID = result.USER_DOC_ID;
+      let originSite = result.ORIGIN_SITE;
+      if (result.USER_DOC_ID){
+        addSettingInteractionHistory(request.domain, originSite, userDocID, request.setting, request.prevSetting, request.newSetting, request.universalSetting, request.location, request.subcollection);
+      } else {
+        console.log("Unregistered user: not connected to the database");
+      }
+    })
+  }
+  if (request.greeting == "LEARNING COMPLETED"){
+    chrome.storage.local.set({"LEARNING": "Just Finished"}, function(){
+      let alreadyOpen = false;
+      let extensionID = chrome.runtime.id;
+      chrome.tabs.query({}, function(tabs) {
+        for (let i = 0, tab; tab = tabs[i]; i++) {
+            if (tab.url===("chrome-extension://"+ extensionID + "/options/options.html")) {
+              chrome.tabs.reload(tab.id, {}, function(){});
+              chrome.tabs.update(tab.id, {active: true});
+              alreadyOpen = true;
+            }
+        }
+      });
+      if (!alreadyOpen){
+        chrome.runtime.openOptionsPage();
+      }
+    })
+  }
+});
 
-// // Set the ORIGIN_SITE property in local storage as current site url for option page
-// chrome.action.onClicked.addListener(function(tab) {
-//   let url = tab.url;
-//   chrome.storage.local.set({"ORIGIN_SITE": url}, ()=>{
-//     chrome.runtime.openOptionsPage(() => {});
-//   });
-// });
+// Set the ORIGIN_SITE property in local storage as current site url for option page
+chrome.action.onClicked.addListener(function(tab) {
+  let url = tab.url;
+  chrome.storage.local.set({"ORIGIN_SITE": url}, ()=>{
+    chrome.runtime.openOptionsPage(() => {});
+  });
+});
 
-// // Enable the extenstion with default sendSignal set to true
-// const enable = () => {
-//   fetch("json/headers.json")
-//     .then((response) => response.text())
-//     .then((value) => {
-//       optout_headers = JSON.parse(value);
-//       // chrome.webRequest.onBeforeRequest.addListener(addDomSignal, {urls: ["<all_urls>"]}, ["blocking"]);
-//       // chrome.webRequest.onBeforeSendHeaders.addListener(addHeaders, {urls: ["<all_urls>"]}, ["requestHeaders", "extraHeaders", "blocking"]);
-//       chrome.storage.local.set({ ENABLED: true });
-//       // setCache(enabled=true)
-//     })
-//     .catch((e) => console.log(`Failed to intialize OptMeowt (JSON load process) (ContentScript): ${e}`));
-// }
+// Enable the extenstion with default sendSignal set to true
+const enable = () => {
+  fetch("json/headers.json")
+    .then((response) => response.text())
+    .then((value) => {
+      optout_headers = JSON.parse(value);
+      // chrome.webRequest.onBeforeRequest.addListener(addDomSignal, {urls: ["<all_urls>"]}, ["blocking"]);
+      // chrome.webRequest.onBeforeSendHeaders.addListener(addHeaders, {urls: ["<all_urls>"]}, ["requestHeaders", "extraHeaders", "blocking"]);
+      chrome.storage.local.set({ ENABLED: true });
+      // setCache(enabled=true)
+    })
+    .catch((e) => console.log(`Failed to intialize OptMeowt (JSON load process) (ContentScript): ${e}`));
+}
 
-// // Function used to set the locally stored values in the cache upon change
-// function setCache(enabled='dontSet', domains='dontSet', domainlistEnabled='dontSet', applyAll='dontSet'){
-//   if(enabled!='dontSet') enabledCache=enabled;
-//   if(domains!='dontSet') {
-//     domainsCache=domains;
-//     updateDomains(Object.keys(domains));
-//   }
-//   if(domainlistEnabled!='dontSet') domainlistEnabledCache=domainlistEnabled;
-//   if(applyAll!='dontSet') applyAllCache=applyAll;
-// }
+// Function used to set the locally stored values in the cache upon change
+function setCache(enabled='dontSet', domains='dontSet', domainlistEnabled='dontSet', applyAll='dontSet'){
+  if(enabled!='dontSet') enabledCache=enabled;
+  if(domains!='dontSet') {
+    domainsCache=domains;
+    updateDomains(Object.keys(domains));
+  }
+  if(domainlistEnabled!='dontSet') domainlistEnabledCache=domainlistEnabled;
+  if(applyAll!='dontSet') applyAllCache=applyAll;
+}
 
-// // Update the sendSignal boolean for the current page
-// async function updateSendSignal(){
-//   await chrome.storage.local.get(["UI_SCHEME"], async function (result) {
-//     let userScheme = result.UI_SCHEME;
-//     if (userScheme == 0 || userScheme == 1 || userScheme==2) updateSendSignalScheme1();
-//     else if (userScheme == 3) updateSendSignalScheme3();
-//     else if (userScheme == 4) updateSendSignalScheme4();
-//     else if (userScheme == 5) updateSendSignalScheme5();
-//     else updateSendSignalScheme6();
-//   })
-// }
+// Update the sendSignal boolean for the current page
+async function updateSendSignal(){
+  await chrome.storage.local.get(["UI_SCHEME"], async function (result) {
+    let userScheme = result.UI_SCHEME;
+    if (userScheme == 0 || userScheme == 1 || userScheme==2) updateSendSignalScheme1();
+    else if (userScheme == 3) updateSendSignalScheme3();
+    else if (userScheme == 4) updateSendSignalScheme4();
+    else if (userScheme == 5) updateSendSignalScheme5();
+    else updateSendSignalScheme6();
+  })
+}
 
-// // SCHEME 0/1/2 : Banner
-// function updateSendSignalScheme1(){
-//   if(domainlistEnabledCache){
-//     if (!(domainsCache[currentDomain]===undefined)) sendSignal=domainsCache[currentDomain]
-//     else{
-//       if (applyAllCache) sendSignal=enabledCache
-//       else sendSignal=false
-//     }
-//   } else sendSignal = enabledCache
-// }
+// SCHEME 0/1/2 : Banner
+function updateSendSignalScheme1(){
+  if(domainlistEnabledCache){
+    if (!(domainsCache[currentDomain]===undefined)) sendSignal=domainsCache[currentDomain]
+    else{
+      if (applyAllCache) sendSignal=enabledCache
+      else sendSignal=false
+    }
+  } else sendSignal = enabledCache
+}
 
-// // SCHEME 4: Questionnaire
-// async function updateSendSignalScheme4(){
-//   if (currentDomain in domainsCache) sendSignal = domainsCache[currentDomain];
-//   else {
-//     await chrome.storage.local.get(["CHECKLIST", "CHECKNOTLIST", "USER_CHOICES"], function(result){
-//       if (result.CHECKLIST.includes(currentDomain)) sendSignal = true;
-//       else{
-//         if (result.USER_CHOICES["Others"] == true && (!(result.CHECKNOTLIST.includes(currentDomain)))) {
-//           sendSignal = true;
-//         }
-//       }
-//     })
-//   }
-//   // console.log("updated signal for " + currentDomain + " is " + sendSignal);
-// }
+// SCHEME 4: Questionnaire
+async function updateSendSignalScheme4(){
+  if (currentDomain in domainsCache) sendSignal = domainsCache[currentDomain];
+  else {
+    await chrome.storage.local.get(["CHECKLIST", "CHECKNOTLIST", "USER_CHOICES"], function(result){
+      if (result.CHECKLIST.includes(currentDomain)) sendSignal = true;
+      else{
+        if (result.USER_CHOICES["Others"] == true && (!(result.CHECKNOTLIST.includes(currentDomain)))) {
+          sendSignal = true;
+        }
+      }
+    })
+  }
+  // console.log("updated signal for " + currentDomain + " is " + sendSignal);
+}
 
-// // SCHEME 3: Privacy Profile
-// async function updateSendSignalScheme3(){
-//   if (currentDomain in domainsCache) sendSignal = domainsCache[currentDomain];
-//   else {
-//     await chrome.storage.local.get(["CHECKLIST", "NPSLIST", "USER_CHOICES"], function(result){
-//       if (result.USER_CHOICES == "Not Privacy-Sensitive"){
-//         if (result.NPSLIST.includes(currentDomain)) sendSignal = true;
-//       } else if (result.USER_CHOICES == "Moderately Privacy-Sensitive"){
-//         if (result.CHECKLIST.includes(currentDomain)) sendSignal = true;
-//       } else {
-//         sendSignal = true;
-//       }
-//     })
-//   }
-//   // console.log("updated signal for " + currentDomain + " is " + sendSignal);
-// }
+// SCHEME 3: Privacy Profile
+async function updateSendSignalScheme3(){
+  if (currentDomain in domainsCache) sendSignal = domainsCache[currentDomain];
+  else {
+    await chrome.storage.local.get(["CHECKLIST", "NPSLIST", "USER_CHOICES"], function(result){
+      if (result.USER_CHOICES == "Not Privacy-Sensitive"){
+        if (result.NPSLIST.includes(currentDomain)) sendSignal = true;
+      } else if (result.USER_CHOICES == "Moderately Privacy-Sensitive"){
+        if (result.CHECKLIST.includes(currentDomain)) sendSignal = true;
+      } else {
+        sendSignal = true;
+      }
+    })
+  }
+  // console.log("updated signal for " + currentDomain + " is " + sendSignal);
+}
 
-// // SCHEME 5: Machine Learning
-// async function updateSendSignalScheme5(){
-//   if(domainlistEnabledCache){
-//     if (!(domainsCache[currentDomain]===undefined)) sendSignal=domainsCache[currentDomain]
-//     else{
-//       if (applyAllCache) sendSignal=enabledCache
-//       else sendSignal=false
-//     }
-//   } else sendSignal = enabledCache
+// SCHEME 5: Machine Learning
+async function updateSendSignalScheme5(){
+  if(domainlistEnabledCache){
+    if (!(domainsCache[currentDomain]===undefined)) sendSignal=domainsCache[currentDomain]
+    else{
+      if (applyAllCache) sendSignal=enabledCache
+      else sendSignal=false
+    }
+  } else sendSignal = enabledCache
 
-//   await chrome.storage.local.get(["SEND_SIGNAL_BANNER", "DO_NOT_SEND_SIGNAL_BANNER"], function (result){
-//     let sendSignalBanner = result.SEND_SIGNAL_BANNER;
-//     let doNotSendSignalBanner = result.DO_NOT_SEND_SIGNAL_BANNER;
-//     if (sendSignalBanner + doNotSendSignalBanner == 5){
-//       chrome.storage.local.set({UI_SCHEME: 3, USER_CHOICES: "Not Privacy-Sensitive"});
-//     }
-//   })
-// }
+  await chrome.storage.local.get(["SEND_SIGNAL_BANNER", "DO_NOT_SEND_SIGNAL_BANNER"], function (result){
+    let sendSignalBanner = result.SEND_SIGNAL_BANNER;
+    let doNotSendSignalBanner = result.DO_NOT_SEND_SIGNAL_BANNER;
+    if (sendSignalBanner + doNotSendSignalBanner == 5){
+      chrome.storage.local.set({UI_SCHEME: 3, USER_CHOICES: "Not Privacy-Sensitive"});
+    }
+  })
+}
 
-// // SCHEME 6: Plain YES/NO to Privacy
-// async function updateSendSignalScheme6(){
-//   await chrome.storage.local.get(["USER_CHOICES"], function (result){
-//     if (result.USER_CHOICES == "Yes, Send Signal") sendSignal = true;
-//     else sendSignal = false;
-//   })
-// }
+// SCHEME 6: Plain YES/NO to Privacy
+async function updateSendSignalScheme6(){
+  await chrome.storage.local.get(["USER_CHOICES"], function (result){
+    if (result.USER_CHOICES == "Yes, Send Signal") sendSignal = true;
+    else sendSignal = false;
+  })
+}
 
-// // Add headers if the sendSignal to true
-// function addHeaders (details)  {
-//   currentDomain = getDomain(details.url);
-//   updateSendSignal();
-//   if (sendSignal) {
-//     // console.log("adding GPC headers to " + currentDomain);
-//     for (let signal in optout_headers) {
-//       let s = optout_headers[signal];
-//       details.requestHeaders.push({ name: s.name, value: s.value });
-//     }
-//     return { requestHeaders: details.requestHeaders };
-//   } else {
-//     // console.log("not adding GPC headers to " + currentDomain);
-//     return { requestHeaders: details.requestHeaders };
-//   }
-// };
+// Add headers if the sendSignal to true
+function addHeaders (details)  {
+  currentDomain = getDomain(details.url);
+  updateSendSignal();
+  if (sendSignal) {
+    // console.log("adding GPC headers to " + currentDomain);
+    for (let signal in optout_headers) {
+      let s = optout_headers[signal];
+      details.requestHeaders.push({ name: s.name, value: s.value });
+    }
+    return { requestHeaders: details.requestHeaders };
+  } else {
+    // console.log("not adding GPC headers to " + currentDomain);
+    return { requestHeaders: details.requestHeaders };
+  }
+};
 
-// // Add dom signal if sendSignal to true
-// function addDomSignal (details)  {
-//   currentDomain = getDomain(details.url);
-//   updateSendSignal();
-//   if (sendSignal) {
-//     // console.log("addding GPC dom signals");
-//     // From DDG, regarding `Injection into non-html pages` on issue-128
-//     try { 
-//       const contentType = document.documentElement.ownerDocument.contentType
-//       // don't inject into xml or json pages
-//       if (contentType === 'application/xml' ||
-//           contentType === 'application/json' ||
-//           contentType === 'text/xml' ||
-//           contentType === 'text/json' ||
-//           contentType === 'text/rss+xml' || 
-//           contentType === 'application/rss+xml'
-//       ) return
-//     } catch (e) {}
-//     chrome.scripting.executeScript(details.tabId, {
-//       file: ["dom.js"],
-//       target: {frameId: details.frameId},
-//       runAt: "document_start",
-//     });
-//   }
-//   // console.log("Not adding GPC dom signals");
-// }
+// Add dom signal if sendSignal to true
+function addDomSignal (details)  {
+  currentDomain = getDomain(details.url);
+  updateSendSignal();
+  if (sendSignal) {
+    // console.log("addding GPC dom signals");
+    // From DDG, regarding `Injection into non-html pages` on issue-128
+    try { 
+      const contentType = document.documentElement.ownerDocument.contentType
+      // don't inject into xml or json pages
+      if (contentType === 'application/xml' ||
+          contentType === 'application/json' ||
+          contentType === 'text/xml' ||
+          contentType === 'text/json' ||
+          contentType === 'text/rss+xml' || 
+          contentType === 'application/rss+xml'
+      ) return
+    } catch (e) {}
+    chrome.scripting.executeScript(details.tabId, {
+      file: ["dom.js"],
+      target: {frameId: details.frameId},
+      runAt: "document_start",
+    });
+  }
+  // console.log("Not adding GPC dom signals");
+}
 
-// // Function used to get the hostname from the current url
-// function getHostName(url) {
-//   let match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
-//   if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) return match[2];
-//   else return null;
-// }
+// Function used to get the hostname from the current url
+function getHostName(url) {
+  let match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+  if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) return match[2];
+  else return null;
+}
 
-// // Function used to get the top level domain for the current url
-// function getDomain(url) {
-//   let hostName = getHostName(url);
-//   let domain = hostName;
+// Function used to get the top level domain for the current url
+function getDomain(url) {
+  let hostName = getHostName(url);
+  let domain = hostName;
   
-//   if (hostName != null) {
-//       let parts = hostName.split('.').reverse();
-//       if (parts != null && parts.length > 1) {
-//           domain = parts[1] + '.' + parts[0];
-//           if (hostName.toLowerCase().indexOf('.co.uk') != -1 && parts.length > 2) {
-//             domain = parts[2] + '.' + domain;
-//           }
-//       }
-//   }
-//   return domain;
-// }
+  if (hostName != null) {
+      let parts = hostName.split('.').reverse();
+      if (parts != null && parts.length > 1) {
+          domain = parts[1] + '.' + parts[0];
+          if (hostName.toLowerCase().indexOf('.co.uk') != -1 && parts.length > 2) {
+            domain = parts[2] + '.' + domain;
+          }
+      }
+  }
+  return domain;
+}
 
 // Open the relevant page based on the schemes
 function openPage(url){
