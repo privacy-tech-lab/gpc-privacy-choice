def1cce5faea2b895a05ed31d372bab33ba68d24
Chunyue Ma <cma01@wesleyan.edu>
2022-01-28 22:36:35 -0500

    fix the icon and option page

--- src/firebase.js
+++ src/firebase.js
@@ -126,7 +126,6 @@ export function updateDomains(domainsList){
 }
 
 // Put list of disconnect domains into array
-
 let disconnectList={};
 let disconnect;
 fetch("json/services.json")
@@ -863,30 +862,6 @@ function addDomSignal (details)  {
   // console.log("Not adding GPC dom signals");
 }
 
-// // Function used to get the hostname from the current url
-// function getHostName(url) {
-//   let match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
-//   if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) return match[2];
-//   else return null;
-// }
-
-// Function used to get the top level domain for the current url
-// function getDomain(url) {
-//   let hostName = getHostName(url);
-//   let domain = hostName;
-  
-//   if (hostName != null) {
-//       let parts = hostName.split('.').reverse();
-//       if (parts != null && parts.length > 1) {
-//           domain = parts[1] + '.' + parts[0];
-//           if (hostName.toLowerCase().indexOf('.co.uk') != -1 && parts.length > 2) {
-//             domain = parts[2] + '.' + domain;
-//           }
-//       }
-//   }
-//   return domain;
-// }
-
 // Open the relevant page based on the schemes
 function openPage(url){
     chrome.tabs.create({
@@ -894,3 +869,6 @@ function openPage(url){
       active: true
     });
 }
+
+
+chrome.action.onClicked.addListener((tab) => openPage("options/options.html"))
