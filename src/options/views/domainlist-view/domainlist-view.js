5f5a8d07d8b00fc60d440a0a6b0f62d4e0e821e9
Chunyue Ma <cma01@wesleyan.edu>
2022-02-21 17:36:10 -0500

    fix: fix the turning on/off of GPC function #176

--- src/options/views/domainlist-view/domainlist-view.js
+++ src/options/views/domainlist-view/domainlist-view.js
@@ -1,6 +1,13 @@
-// GPC Privacy Choice is licensed under the MIT License
-// Copyright (c) 2021 Chunyue Ma, Isabella Tassone, Eliza Kuller, Sebastian Zimmeck
-// privacy-tech-lab, https://privacytechlab.org/
+/*
+GPC Privacy Choice is licensed under the MIT License
+Copyright (c) 2021 Chunyue Ma, Isabella Tassone, Eliza Kuller, Sebastian Zimmeck
+privacy-tech-lab, https://privacytechlab.org/
+
+This file is the main driver file which communicates with the background service worker. 
+It exports the domainListView function, which builds the UI for the domain list page, as well as adding the event listeners responding to the users interaction with the page
+
+users actions on domains-list/options page --- chrome runtime --> background.js service worker
+*/
 
 import { renderParse, fetchParse } from "../../components/util.js";
 import {
@@ -8,13 +15,70 @@ import {
   deleteDomain,
   allOn,
   allOff,
-  turnOnGPC,
+  addToggleListeners,
+  handleToggleAllOn, 
+  handleToggleAllOff, 
+  handleApplyAllSwitch, 
+  handleDeleteDomainListEvent,
+  handleFutureSettingPromptEvent
 } from "./domainlist-util.js";
 
 const domainListHeadings = {
   title: "Global Privacy Control (GPC) Settings",
   subtitle: "Review or Modify Your GPC Settings",
 };
+
+// Renders the `domain list` view in the options page
+export async function domainlistView(scaffoldTemplate, buildList) {
+  let body;
+  let content;
+
+  if (buildList) {
+    body = renderParse(
+      scaffoldTemplate,
+      domainListHeadings,
+      "scaffold-component"
+    );
+    content = await fetchParse(
+      "./views/domainlist-view/domainlist-view.html",
+      "domainlist-view"
+    );
+  } else {
+    body = renderParse(
+      scaffoldTemplate,
+      domainListHeadings,
+      "scaffold-component"
+    );
+    content = await fetchParse(
+      "./views/domainlist-view/domainlist-view-plain.html",
+      "domainlist-view-plain"
+    );
+  }
+
+  document.getElementById("content").innerHTML = body.innerHTML;
+  document.getElementById("scaffold-component-body").innerHTML =
+    content.innerHTML;
+
+  createDefaultSettingInfo();
+
+  if (buildList) {
+    createDomainlistManagerButtons();
+    createList();
+  }
+  addEventListeners();
+  chrome.storage.local.get(["LEARNING"], function (result) {
+    if (result.LEARNING == "Just Finished") {
+      chrome.storage.local.set({ LEARNING: "Completed" }, function () {
+        let modal = UIkit.modal("#learning-finish-modal");
+        modal.show();
+        document.getElementById("learning-finish-modal-button").onclick =
+          function () {
+            modal.hide();
+          };
+      });
+    }
+  });
+}
 
 // Creates all the event listeners for the options page buttons
 function addEventListeners() {
@@ -45,331 +109,6 @@ function addEventListeners() {
       handleDeleteDomainListEvent();
     }
   });
-}
-
-// Creates the specific Domain List toggles as well as the perm delete
-function addToggleListeners() {
-  chrome.storage.local.get(["DOMAINS"], function (result) {
-    for (let domain in result.DOMAINS) {
-      addDomainToggleListener(domain, domain);
-      addDeleteButtonListener(domain);
-    }
-  });
-}
-
-// Turn on / off the domain from the setting page
-function addDomainToggleListener(elementId, domain) {
-  document.getElementById(elementId).addEventListener("click", () => {
-    chrome.storage.local.set(
-      { ENABLED: true, DOMAINLIST_ENABLED: true },
-      function () {
-        chrome.storage.local.get(
-          ["DOMAINS", "UV_SETTING", "UI_SCHEME"],
-          function (result) {
-            console.log(result.DOMAINS[domain])
-            if (result.DOMAINS[domain] == true) {
-              let new_domains = [];
-              chrome.storage.local.get(["DOMAINS"], function (result) {
-                new_domains = result.DOMAINS;
-                new_domains[domainKey] = false;
-                console.log("Seeting new_domains[domainKey] to, ", new_domains[domainKey])
-                chrome.storage.local.set({ DOMAINS: new_domains });
-              });
-              chrome.runtime.sendMessage({greeting: "DOMAIN SPECIFIC GPC OFF", domainKey: domain});
-              if (result.UI_SCHEME == 1) {
-                chrome.runtime.sendMessage({
-                  greeting: "INTERACTION",
-                  domain: domain,
-                  setting: "GPC signal",
-                  prevSetting: "Don't allow tracking",
-                  newSetting: "Allow tracking",
-                  universalSetting: result.UV_SETTING,
-                  location: "Options page",
-                  subcollection: "Domain",
-                });
-              } else {
-                chrome.runtime.sendMessage({
-                  greeting: "INTERACTION",
-                  domain: domain,
-                  setting: "GPC signal",
-                  prevSetting: "Don't allow tracking",
-                  newSetting: "Allow tracking",
-                  universalSetting: null,
-                  location: "Options page",
-                  subcollection: "Domain",
-                });
-              }
-            } else {
-              let new_domains = [];
-              chrome.storage.local.get(["DOMAINS"], function (result) {
-                new_domains = result.DOMAINS;
-                new_domains[domainKey] = true;
-                console.log("Seeting new_domains[domainKey] to, ", new_domains[domainKey])
-                chrome.storage.local.set({ DOMAINS: new_domains });
-              });
-              chrome.runtime.sendMessage({greeting: "DOMAIN SPECIFIC GPC ON", domainKey: domain});
-              if (result.UI_SCHEME == 1) {
-                chrome.runtime.sendMessage({
-                  greeting: "INTERACTION",
-                  domain: domain,
-                  setting: "GPC signal",
-                  prevSetting: "Allow tracking",
-                  newSetting: "Don't allow tracking",
-                  universalSetting: result.UV_SETTING,
-                  location: "Options page",
-                  subcollection: "Domain",
-                });
-              } else {
-                chrome.runtime.sendMessage({
-                  greeting: "INTERACTION",
-                  domain: domain,
-                  setting: "GPC signal",
-                  prevSetting: "Allow tracking",
-                  newSetting: "Don't allow tracking",
-                  universalSetting: null,
-                  location: "Options page",
-                  subcollection: "Domain",
-                });
-              }
-            }
-          }
-        );
-      }
-    );
-  });
-}
-
-// Toggle delete buttons for each domain
-function addDeleteButtonListener(domain) {
-  document.getElementById(`delete ${domain}`).addEventListener("click", () => {
-    deleteDomain(domain);
-    document.getElementById(`li ${domain}`).remove();
-    chrome.storage.local.get(["UV_SETTING"], function (result) {
-      chrome.runtime.sendMessage({
-        greeting: "INTERACTION",
-        domain: domain,
-        setting: "Delete domain",
-        prevSetting: null,
-        newSetting: null,
-        universalSetting: result.UV_SETTING,
-        location: "Options page",
-        subcollection: "Domain",
-      });
-    });
-  });
-}
-
-// "Do not allow tracking for all" button is clicked
-function handleToggleAllOn() {
-  // "Apply all" box isn't checked
-  let toggleOn_prompt = `Are you sure you would like to toggle on the GPC setting for all sites on the website list? NOTE: Your current preferences will be overwritten.`;
-  if (confirm(toggleOn_prompt)) {
-    chrome.storage.local.get(["DOMAINS", "UV_SETTING"], function (result) {
-      if (allOff(result.DOMAINS) === false && allOn(result.DOMAINS) !== true) {
-        chrome.runtime.sendMessage({
-          greeting: "INTERACTION",
-          domain: "All existing domains",
-          setting: "GPC signal",
-          prevSetting: "Personalized domain list",
-          newSetting: "Send signal",
-          universalSetting: result.UV_SETTING,
-          location: "Options page",
-          subcollection: "Domain",
-        });
-      } else if (allOff(result.DOMAINS) === true) {
-        chrome.runtime.sendMessage({
-          greeting: "INTERACTION",
-          domain: "All existing domains",
-          setting: "GPC signal",
-          prevSetting: "Don't send signal",
-          newSetting: "Send signal",
-          universalSetting: result.UV_SETTING,
-          location: "Options page",
-          subcollection: "Domain",
-        });
-      } else {
-        chrome.runtime.sendMessage({
-          greeting: "INTERACTION",
-          domain: "All existing domains",
-          setting: "GPC Signal",
-          prevSetting: "Send signal",
-          newSetting: "No change",
-          universalSetting: result.UV_SETTING,
-          location: "Options page",
-          subcollection: "Domain",
-        });
-      }
-      let new_domains = result.DOMAINS;
-      for (let d in new_domains) {
-        new_domains[d].bool = true;
-      }
-      chrome.runtime.sendMessage({
-        greeting: "UPDATE CACHE",
-        newEnabled: "dontSet",
-        newDomains: new_domains,
-        newDomainlistEnabled: "dontSet",
-      });
-      chrome.storage.local.set({ DOMAINS: new_domains });
-      createList();
-      addToggleListeners();
-    });
-  }
-}
-
-// "Allow tracking for all" button is clicked
-function handleToggleAllOff() {
-  let toggleOff_prompt = `Are you sure you would like to toggle off the GPC setting for all sites on the website list? NOTE: Your current preferences will be overwritten.`;
-  if (confirm(toggleOff_prompt)) {
-    chrome.storage.local.get(["DOMAINS", "UV_SETTING"], function (result) {
-      if (allOn(result.DOMAINS) === false && allOff(result.DOMAINS) === false) {
-        chrome.runtime.sendMessage({
-          greeting: "INTERACTION",
-          domain: "All existing domains",
-          setting: "GPC Signal",
-          prevSetting: "Personalized domain list",
-          newSetting: "Don't send signal",
-          universalSetting: result.UV_SETTING,
-          location: "Options page",
-          subcollection: "Domain",
-        });
-      } else if (allOn(result.DOMAINS) === true) {
-        chrome.runtime.sendMessage({
-          greeting: "INTERACTION",
-          domain: "All existing domains",
-          setting: "GPC Signal",
-          prevSetting: "Send signal",
-          newSetting: "Don't send signal",
-          universalSetting: result.UV_SETTING,
-          location: "Options page",
-          subcollection: "Domain",
-        });
-      } else {
-        chrome.runtime.sendMessage({
-          greeting: "INTERACTION",
-          domain: "All existing domains",
-          setting: "GPC Signal",
-          prevSetting: "Don't send signal",
-          newSetting: "No change",
-          universalSetting: result.UV_SETTING,
-          location: "Options page",
-          subcollection: "Domain",
-        });
-      }
-      let new_domains = result.DOMAINS;
-      for (let d in new_domains) {
-        new_domains[d].bool = false;
-      }
-      chrome.storage.local.set({ DOMAINS: new_domains });
-      chrome.runtime.sendMessage({
-        greeting: "UPDATE CACHE",
-        newEnabled: "dontSet",
-        newDomains: new_domains,
-        newDomainlistEnabled: "dontSet",
-      });
-      createList();
-      addToggleListeners();
-    });
-  }
-}
-
-// "Apply all" switch is hit
-function handleApplyAllSwitch() {
-  chrome.storage.local.get(["UV_SETTING", "APPLY_ALL"], function (result) {
-    if (result.APPLY_ALL) {
-      chrome.runtime.sendMessage({
-        greeting: "INTERACTION",
-        domain: "All future domains",
-        setting: "Universal Setting",
-        prevSetting: result.UV_SETTING,
-        newSetting: "Off",
-        universalSetting: "Off",
-        location: "Options page",
-        subcollection: "Domain",
-      });
-      chrome.storage.local.set({ DOMAINLIST_ENABLED: true });
-      chrome.storage.local.set({ UV_SETTING: "Off" });
-      chrome.storage.local.set({ APPLY_ALL: false });
-      chrome.storage.local.set({ ENABLED: true });
-      createDefaultSettingInfo();
-      chrome.runtime.sendMessage({
-        greeting: "UPDATE CACHE",
-        newEnabled: true,
-        newDomains: "dontSet",
-        newDomainlistEnabled: true,
-      });
-    } else {
-      createDefaultSettingInfo();
-      UIkit.modal("#future_setting_prompt").show();
-      createDefaultSettingInfo();
-    }
-  });
-}
-
-// User interacts with future setting prompt, shown when users attempt to turn on the "Apply all" switch
-function handleFutureSettingPromptEvent(event) {
-  // User hits "Allow tracking for all"
-  if (event.target.id == "allow-future-btn") {
-    chrome.storage.local.set({ APPLY_ALL: true });
-    chrome.storage.local.set({ UV_SETTING: "Don't send signal to all" });
-    chrome.storage.local.set({ ENABLED: false });
-    createDefaultSettingInfo();
-    chrome.runtime.sendMessage({
-      greeting: "INTERACTION",
-      domain: "All future domains",
-      setting: "Universal Setting",
-      prevSetting: "Off",
-      newSetting: "Don't send signal to all",
-      universalSetting: "Don't send signal to all",
-      location: "Options page",
-      subcollection: "Domain",
-    });
-  }
-  // User hits "Don't allow tracking" for all
-  else if (event.target.id == "dont-allow-future-btn") {
-    chrome.storage.local.set({ APPLY_ALL: true });
-    chrome.storage.local.set({ UV_SETTING: "Send signal to all" });
-    chrome.storage.local.set({ ENABLED: true });
-    createDefaultSettingInfo();
-    chrome.runtime.sendMessage({
-      greeting: "INTERACTION",
-      domain: "All future domains",
-      setting: "Universal Setting",
-      prevSetting: "Off",
-      newSetting: "Send signal to all",
-      universalSetting: "Send signal to all",
-      location: "Options page",
-      subcollection: "Domain",
-    });
-  }
-  // Otherwise, they hit cancel and nothing changes
-}
-
-// Entire domain list is deleted
-function handleDeleteDomainListEvent() {
-  let delete_prompt = `Are you sure you would like to permanently delete all domains from the Domain List? NOTE: Domains will be automatically added back to the list when the domain is requested again.`;
-  if (confirm(delete_prompt)) {
-    chrome.storage.local.get(["UV_SETTING"], function (result) {
-      chrome.runtime.sendMessage({
-        greeting: "INTERACTION",
-        domain: "All existing domains",
-        setting: "Delete domain",
-        prevSetting: null,
-        newSetting: null,
-        universalSetting: result.UV_SETTING,
-        location: "Options page",
-        subcollection: "Domain",
-      });
-    });
-    chrome.storage.local.set({ DOMAINS: {} });
-    chrome.runtime.sendMessage({
-      greeting: "UPDATE CACHE",
-      newEnabled: "dontSet",
-      newDomains: {},
-      newDomainlistEnabled: "dontSet",
-    });
-  }
-  createList();
-  addToggleListeners();
 }
 
 // User changes GPC signal send status on scheme 6
@@ -1204,54 +943,4 @@ async function updatePrefScheme4() {
   );
 }
 
-// Renders the `domain list` view in the options page
-export async function domainlistView(scaffoldTemplate, buildList) {
-  let body;
-  let content;
 
-  if (buildList) {
-    body = renderParse(
-      scaffoldTemplate,
-      domainListHeadings,
-      "scaffold-component"
-    );
-    content = await fetchParse(
-      "./views/domainlist-view/domainlist-view.html",
-      "domainlist-view"
-    );
-  } else {
-    body = renderParse(
-      scaffoldTemplate,
-      domainListHeadings,
-      "scaffold-component"
-    );
-    content = await fetchParse(
-      "./views/domainlist-view/domainlist-view-plain.html",
-      "domainlist-view-plain"
-    );
-  }
-
-  document.getElementById("content").innerHTML = body.innerHTML;
-  document.getElementById("scaffold-component-body").innerHTML =
-    content.innerHTML;
-
-  createDefaultSettingInfo();
-
-  if (buildList) {
-    createDomainlistManagerButtons();
-    createList();
-  }
-  addEventListeners();
-  chrome.storage.local.get(["LEARNING"], function (result) {
-    if (result.LEARNING == "Just Finished") {
-      chrome.storage.local.set({ LEARNING: "Completed" }, function () {
-        let modal = UIkit.modal("#learning-finish-modal");
-        modal.show();
-        document.getElementById("learning-finish-modal-button").onclick =
-          function () {
-            modal.hide();
-          };
-      });
-    }
-  });
-}
