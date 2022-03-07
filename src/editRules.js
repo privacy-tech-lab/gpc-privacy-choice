c0491ae5bb40b05fa597eeeda66f92752fa230e4
ekuller <77850710+ekuller@users.noreply.github.com>
2022-03-06 20:45:48 -0500

    Issue 171 c (#180)
    * Issue 178 (#179)
    
    * feat: create endpoints for service worker listner #178
    
    * chore: standardize request.domain and id
    
    * chore: deprecate setCache #178
    
    * refactor: remove updateCache function
    
    * feat: finish up the rule set integration (level 1)
    
    * fix: add in endpoints for banner global reaction
    
    * add function for modifying domains condition
    
    * implement adding domains to dynamic rule with domains condition
    
    * update APIs
    
    * update APIs
    
    * add function to get unique id from string
    
    * address errors
    
    * update clearRules
    
    * update clearRules
    
    * make api calls in profile scheme
    
    Co-authored-by: Chunyue Ma <57780561+chunyuema@users.noreply.github.com>

--- src/editRules.js
+++ src/editRules.js
@@ -1,38 +1,132 @@
-// Add a new rule with id to the rule set
-export async function addRule(domain, id, priority) {
-  chrome.declarativeNetRequest.updateDynamicRules({
-    addRules: [
-      {
-        id: id,
-        priority: priority,
-        action: {
-          type: "modifyHeaders",
-          requestHeaders: [
-            { header: "Sec-GPC", operation: "set", value: "1" },
-            { header: "DNT", operation: "set", value: "1" },
-          ],
-        },
-        condition: {
-          urlFilter: domain,
-          resourceTypes: ["main_frame"],
-        },
-      },
-    ],
-  });
-  chrome.declarativeNetRequest.getDynamicRules((rules) => console.log(rules));
-  console.log("New rule added with id", id);
+function getIdFromUrl(url) {
+	String.prototype.hashCode = function () {
+		var hash = 0,
+			i,
+			chr;
+		if (this.length === 0) return hash;
+		for (i = 0; i < this.length; i++) {
+			chr = this.charCodeAt(i);
+			hash = (hash << 5) - hash + chr;
+			hash |= 0; // Convert to 32bit integer
+		}
+		return hash;
+	};
+	return url.hashCode();
+}
+
+// Add a new rule with id to the rule set that adds gpc to requests being sent to domain
+export async function addUrlRule(domain) {
+	let id = getIdFromUrl(domain);
+	chrome.declarativeNetRequest.updateDynamicRules({
+		addRules: [
+			{
+				id: id,
+				priority: 3,
+				action: {
+					type: "modifyHeaders",
+					requestHeaders: [
+						{ header: "Sec-GPC", operation: "set", value: "1" },
+						{ header: "DNT", operation: "set", value: "1" },
+					],
+				},
+				condition: {
+					urlFilter: domain,
+					resourceTypes: ["main_frame"],
+				},
+			},
+		],
+	});
+	chrome.declarativeNetRequest.getDynamicRules((rules) => console.log(rules));
+	console.log("New rule added with id", id);
+}
+
+// Get list of originating domains where GPC is enabled
+function getEnabledDomains() {
+	let domains = chrome.declarativeNetRequest.getDynamicRules((res) => {
+		let rule = res.filter((obj) => {
+			return obj.id === 2;
+		});
+		if (rule.length === 0) {
+			return [];
+		} else {
+			return rule[0].conditions.domains;
+		}
+	});
+	return domains;
+}
+
+// Add a new rule with id to the rule set that adds gpc to requests originating from a set of domains
+export async function addDomainRule(domain) {
+	chrome.declarativeNetRequest.updateDynamicRules({
+		addRules: [
+			{
+				id: 2,
+				priority: 1,
+				action: {
+					type: "modifyHeaders",
+					requestHeaders: [
+						{ header: "Sec-GPC", operation: "set", value: "1" },
+						{ header: "DNT", operation: "set", value: "1" },
+					],
+				},
+				condition: {
+					domains: getEnabledDomains().push(domain),
+					resourceTypes: ["main_frame"],
+				},
+			},
+		],
+		removeRuleIds: [2],
+	});
+	chrome.declarativeNetRequest.getDynamicRules((rules) => console.log(rules));
+	console.log("Rule modified with id", id);
+}
+
+export async function removeDomainFromRule(domain) {
+	let domains = await getEnabledDomains();
+	for (var i = domains.length - 1; i >= 0; i--) {
+		if (domains[i] === domain) {
+			domains.splice(i, 1);
+		}
+	}
+	chrome.declarativeNetRequest.updateDynamicRules({
+		addRules: [
+			{
+				id: 2,
+				priority: 1,
+				action: {
+					type: "modifyHeaders",
+					requestHeaders: [
+						{ header: "Sec-GPC", operation: "set", value: "1" },
+						{ header: "DNT", operation: "set", value: "1" },
+					],
+				},
+				condition: {
+					domains: domains,
+					resourceTypes: ["main_frame"],
+				},
+			},
+		],
+		removeRuleIds: [2],
+	});
+	chrome.declarativeNetRequest.getDynamicRules((rules) => console.log(rules));
+	console.log("rule modified with id", id);
 }
 
 // Remove a rule with id from the rule set
-export async function rmRule(id) {
-  console.log("Remove rule with id", id);
-  chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: [id] });
-  chrome.declarativeNetRequest.getDynamicRules((rules) => console.log(rules));
+export async function rmRule(url) {
+	let id = await getIdFromUrl(url);
+	console.log("Remove rule with id", id);
+	chrome.declarativeNetRequest.updateDynamicRules({ removeRuleIds: [id] });
+	chrome.declarativeNetRequest.getDynamicRules((rules) => console.log(rules));
 }
 
 // Clear all rules from the rule set
-export async function clearRules(ruleIds) {
-  for (let id in ruleIds) {
-    rmRule(id);
-  }
+export async function clearRules() {
+	chrome.declarativeNetRequest.getDynamicRules((rules) => {
+		for (let r in rules) {
+			chrome.declarativeNetRequest.updateDynamicRules({
+				removeRuleIds: [rules[r].id],
+			});
+		}
+	});
 }
